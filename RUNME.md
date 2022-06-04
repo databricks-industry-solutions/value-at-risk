54723d48de33d7c63f0fe208c644a1829e4a333d
Antoine Amend <antoine.amend@gmail.com>
2022-06-04 11:18:08 -0700

    unit tests

---
+++ RUNME.md
@@ -0,0 +1,8 @@
+# User Guide
+
+This guide walks you through the configuration and execution of the value at risk solution accelerator.
+Based on previous implementations, we estimate the time for an overall POC duration to be around 10 days.
+
+# Issues
+
+For any issue, please raise ticket to github project directly.
--- utils/var_utils.py
+++ utils/var_utils.py
@@ -1,62 +1,62 @@
 def download_market_data(tick, min_date, max_date):
-  import pandas as pd
-  import yfinance as yf
-  msft = yf.Ticker(tick)
-  raw = msft.history(start=min_date, end=max_date)[['Open', 'High', 'Low', 'Close', 'Volume']]
-  # fill in missing business days
-  idx = pd.date_range(min_date, max_date, freq='B')
-  # use last observation carried forward for missing value
-  output_df = raw.reindex(idx, method='pad')
-  # Pandas does not keep index (date) when converted into spark dataframe
-  output_df['date'] = output_df.index
-  output_df['ticker'] = tick
-  output_df = output_df.rename(columns={"Open": "open", "High": "high", "Low": "low", "Volume": "volume", "Close": "close"})
-  return output_df
+    import pandas as pd
+    import yfinance as yf
+    msft = yf.Ticker(tick)
+    raw = msft.history(start=min_date, end=max_date)[['Open', 'High', 'Low', 'Close', 'Volume']]
+    # fill in missing business days
+    idx = pd.date_range(min_date, max_date, freq='B')
+    # use last observation carried forward for missing value
+    output_df = raw.reindex(idx, method='pad')
+    # Pandas does not keep index (date) when converted into spark dataframe
+    output_df['date'] = output_df.index
+    output_df['ticker'] = tick
+    output_df = output_df.rename(
+        columns={"Open": "open", "High": "high", "Low": "low", "Volume": "volume", "Close": "close"})
+    return output_df
 
 
 def generate_prices(start_price, mu, sigma, days):
-  from scipy import stats
-  import numpy as np
-  shock = np.zeros(days)
-  price = np.zeros(days)
-  sample_rate = 1/float(days)
-  price[0] = start_price
-  for i in range(1, days):
-      shock[i] = np.random.normal(loc=mu * sample_rate, scale=sigma * np.sqrt(sample_rate))
-      price[i] = max(0, price[i - 1] + shock[i] * price[i - 1])
-  return price
+    import numpy as np
+    shock = np.zeros(days)
+    price = np.zeros(days)
+    sample_rate = 1 / float(days)
+    price[0] = start_price
+    for i in range(1, days):
+        shock[i] = np.random.normal(loc=mu * sample_rate, scale=sigma * np.sqrt(sample_rate))
+        price[i] = max(0, price[i - 1] + shock[i] * price[i - 1])
+    return price
 
 
 def create_seed_df(runs):
-  import pandas as pd
-  import numpy as np
-  return pd.DataFrame(list(np.arange(0, runs)), columns = ['trial_id'])
+    import pandas as pd
+    import numpy as np
+    return pd.DataFrame(list(np.arange(0, runs)), columns=['trial_id'])
 
 
 def get_shortfall(simulations, var):
-  import numpy as np
-  var = get_var(simulations, var)
-  return float(np.mean([s for s in simulations if s <= var]))
+    import numpy as np
+    var = get_var(simulations, var)
+    return float(np.mean([s for s in simulations if s <= var]))
 
 
 def get_var(simulations, var):
-  import numpy as np
-  return float(np.percentile(simulations, 100-var))
+    import numpy as np
+    return float(np.percentile(simulations, 100 - var))
 
 
 def non_linear_features(xs):
-  import numpy as np
-  fs = []
-  for x in xs:
-    fs.append(x)
-    fs.append(np.sign(x) * x**2)
-    fs.append(x**3)
-    fs.append(np.sign(x) * np.sqrt(abs(x)))
-  return fs
+    import numpy as np
+    fs = []
+    for x in xs:
+        fs.append(x)
+        fs.append(np.sign(x) * x ** 2)
+        fs.append(x ** 3)
+        fs.append(np.sign(x) * np.sqrt(abs(x)))
+    return fs
 
 
 def predict_non_linears(ps, fs):
     s = ps[0]
     for i, f in enumerate(fs):
-      s = s + ps[i + 1] * f
+        s = s + ps[i + 1] * f
     return float(s)
