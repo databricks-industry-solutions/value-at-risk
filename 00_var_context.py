14516315456d1eda3cd7120c3a11e5e3e74f8a0a
Antoine Amend <aamend@users.noreply.github.com>
2022-07-06 12:39:09 -0700

    Update image

--- 00_var_context.py
+++ 00_var_context.py
@@ -14,7 +14,7 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC <img src='https://github.com/databricks-industry-solutions/value-at-risk/blob/master/images/reference_architecture.png?raw=true' width=800>
+# MAGIC <img src='https://github.com/databricks-industry-solutions/value-at-risk/blob/master/images/reference_architecture.png' width=800>
 
 # COMMAND ----------
 
