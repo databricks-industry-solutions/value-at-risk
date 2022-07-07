936ac1172b989fc6b74a30415c00bea6527d181e
antoine.amend@gmail.com <antoine.amend@gmail.com>
2022-07-07 03:57:55 +0000

    experiment path

--- config/configure_notebook.py
+++ config/configure_notebook.py
@@ -41,7 +41,7 @@ with open('config/indicators.json', 'r') as f:
 
 import mlflow
 username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-mlflow.set_experiment('/Users/{}/var'.format(username))
+mlflow.set_experiment('/Users/{}/value_at_risk'.format(username))
 
 # COMMAND ----------
 
