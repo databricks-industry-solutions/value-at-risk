cb24c0a962ccca4f8cbc669641301c5d2f043d52
antoine.amend@gmail.com <antoine.amend@gmail.com>
2022-07-07 01:09:03 +0000

    align to solutions

--- config/configure_notebook.py
+++ config/configure_notebook.py
@@ -41,7 +41,7 @@ with open('config/indicators.json', 'r') as f:
 
 import mlflow
 username = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
-mlflow.set_experiment('/Users/{}/{}'.format(username, config['model']['name']))
+mlflow.set_experiment('/Users/{}/var'.format(username))
 
 # COMMAND ----------
 
